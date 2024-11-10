2f66d2a9e29c210b327acc50e503a519da55b97e
unknown <rakeshbsirvi46@gmail.com>
2024-11-10 09:01:45 +0530

    Removed frontend files

--- src/components/LandingPage/HeroSection/Types.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react'
-
-
-export interface FloatingElementProps {
-  position: 'left' | 'right' | 'top-left' | 'bottom-right';
-  children: React.ReactNode;
-  className?: string;
-}
